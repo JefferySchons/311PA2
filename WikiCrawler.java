778439c7d897b09676888da86a74a44a5c2a0826
erelsbernd <erelsbernd@gmail.com>
2017-04-03 20:05:11 -0500

    fixed some bugs

--- WikiCrawler.java
+++ WikiCrawler.java
@@ -33,6 +33,8 @@ public class WikiCrawler {
 		added_Url=seedURL;
 		max_pages=max;
 		file_to_Write=fileName;
+		master_list_O_links.add(seedURL);
+		this.extractLinks(seedURL);
 		//A string seedUrl{relative address of the seed url (within Wiki domain).
 		//An integer max representing Maximum number pages to be crawled (number of pages seen (all links gathered)(stop as soon as at max)
 		//A string fileName representing name of a file-The graph will be written to this file
@@ -75,92 +77,42 @@ public class WikiCrawler {
 			}
 			in.close();
 			
-			printToText(list_O_links, doc);
+			addToMasterList(list_O_links, doc);
 		}catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-		//returns arraylist of liks on this page
+		//returns arraylist of links on this page
 		//return null;
-		//now craw
+		//now crawl
 		//if(master_list_O_links.size()<max_pages) //== list would be full
 		//{
-			//this will caouse a looping back and forth between crawl and extractLinks
+			//this will cause a looping back and forth between crawl and extractLinks
 			//tell master_list_O_links==max_pages;
 			crawl();
 		//}
 	}
-	private static void printToText(ArrayList<String> List_O_links, String doc)
+	private static void addToMasterList(ArrayList<String> List_O_links, String doc)
 	{
-		//this class both updates the master_list_O_links and prints to text file all the links
-		//the text file is a 
+		//this class updates the master_list_O_links
 		//add list_O_links to master_list_O_links
-		/*
-		if(master_list_O_links==null)
+		/*  //master list is initialized at start, so can't be null
+		 *  //if(master_list_O_links==null)
+		 */
+		//loop through List_O_links and master_list_O_links
+		for(int i=0; i<List_O_links.size(); i++)
 		{
-			counter=0;
-			master_list_O_links.add(doc);// to add very first link go to
-		}
-		*/
-		
-		
-		/////
-		try(FileWriter fw = new FileWriter("outfilename.txt", true); // will be in folder with .classpath
-				BufferedWriter bw = new BufferedWriter(fw);
-				PrintWriter out = new PrintWriter(bw))
-		{
-			System.out.println("print over link_list_ ");
-			if(master_list_O_links==null)
+			if(!master_list_O_links.contains(List_O_links.get(i)))
 			{
-				counter=0;
-				master_list_O_links.add(doc);// to add very first link go to
-				if(List_O_links.size()<max_pages-1)
+				if(master_list_O_links.size()<max_pages)
 				{
-					master_list_O_links.addAll(List_O_links);
-				}
-				else
-				{
-					//get sub array of List_O_links
-					master_list_O_links.addAll(List_O_links.subList(0,  max_pages-1));
-				}
-				//print
-				for(int j=0; j<master_list_O_links.size(); j++)
-				{
-					out.println(doc + " " + master_list_O_links.get(j).toString());
-					//print link doc -> List_O_links.get(i)
+					master_list_O_links.add(List_O_links.get(i));
 				}
 			}
-			else
-			{
-				//loop through List_O_links and master_list_O_links
-				for(int i=0; i<List_O_links.size(); i++)
-				{
-					if(master_list_O_links.contains(List_O_links.get(i)))
-					{
-						//print link doc -> List_O_links.get(i)
-						out.println(doc + " " + List_O_links.get(i).toString());
-					}
-					else
-					{
-						if(master_list_O_links.size()<max_pages)
-						{
-							master_list_O_links.add(List_O_links.get(i));
-							////print link doc -> List_O_links.get(i)
-							out.println(doc + " " + List_O_links.get(i).toString());
-						}
-						else
-						{
-							//would be new page so ignore
-						}
-					}
-				}
-			}
-		}catch (IOException e) {
-			//exception handling left as an exercise for the reader
 		}
-		/*
 		
+		/*
 		need to do( if != null)  
 		check if links are in master
 			-no 
@@ -173,50 +125,44 @@ public class WikiCrawler {
 				print doc -> link 
 		*/
 		////////
-		/*
-		int master_list_pre_size=master_list_O_links.size();
-		master_list_O_links.addAll(List_O_links);
 		
 		//so i dont get more then max pages
+		//should just remove the back extra pages
 		if(master_list_O_links.size()>=max_pages)
 		{
-			ArrayList<String> correct_size_master= new ArrayList<String>();
-			for(int j=0; j<max_pages; j++)
-			{
-				correct_size_master.add(master_list_O_links.get(j));
-			}
-			master_list_O_links=correct_size_master;
+			//master_list_O_links = (ArrayList<String>) master_list_O_links.subList(0, max_pages);
+			master_list_O_links.subList(max_pages,master_list_O_links.size()).clear();
 		}
-		int master_list_post_size=master_list_O_links.size();
-		//print add to text file form <doc>< List_O_links[i]>
-		
-		
-		try(FileWriter fw = new FileWriter("outfilename.txt", true); // will be in folder with .classpath
-			BufferedWriter bw = new BufferedWriter(fw);
-			PrintWriter out = new PrintWriter(bw))
+	}
+	
+
+	private static void printToText()
+	{
+		//this class prints to text file all the links
+		//the text file is a 
+		/////
+		try(FileWriter fw = new FileWriter("test/_WikiCrawlerTest_crawl_results.txt", true); // will be in folder with .classpath
+				BufferedWriter bw = new BufferedWriter(fw);
+				PrintWriter out = new PrintWriter(bw))
 		{
-			//print to file
-			//for(int i = 0; i<List_O_links.size(); i++)
-			//{
-			//	out.println(doc + " " + List_O_links.get(i).toString());
-			//}
-			for(int i= master_list_pre_size; i<master_list_post_size; i++)
-			{
-				out.println(doc + " " + master_list_O_links.get(i).toString());
-			}
 			
-		} catch (IOException e) {
+			out.println(String.valueOf(master_list_O_links.size()));
+			//loop through List_O_links and master_list_O_links
+			for(int i=0; i<master_list_O_links.size(); i++)
+			{
+				master_list_O_links.contains(master_list_O_links.get(i));
+			}
+		}catch (IOException e) {
 			//exception handling left as an exercise for the reader
 		}
-		*/
-		
 	}
+	
 	public static void crawl()
 	{
-		if(counter%25==0) //wait 1 secound every 25 requests so it waits > 3sec/100 requests
+		if(counter%100==0) //wait 4 second every 100 requests so it waits > 3sec/100 requests
 		{
 				try {
-					Thread.sleep(1000);
+					Thread.sleep(4000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
@@ -230,6 +176,7 @@ public class WikiCrawler {
 			extractLinks(new_link);
 		}
 		//do extract links over links on page
+		printToText();
 	}
 	public ArrayList<String> masterList()
 	{
